719a1e3aa55a0da00e3d3d686573647ec5071027
Matthias Gattermeier <matthias.gattermeier@nytimes.com>
2016-11-22 17:10:04 -0500

    basic component skeleton

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react'
+import ReactDOM from 'react-dom';
+
+export default class SafeImage extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
