ef06f1cb99f9867214f1d0bbeb01f11e7cdc58f1
Matthias Gattermeier <Gattermeier@users.noreply.github.com>
2016-11-22 17:39:56 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import ReactDOM from 'react-dom';
 
-export default class SafeImage extends Component {
+export default class Test extends Component {
   constructor(props) {
     super(props)
   }
